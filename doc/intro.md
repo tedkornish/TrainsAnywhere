0ad4574902df1dbba13abdade1fa2c020e935637
Ted Kornish <ted.kornish+github@gmail.com>
2016-02-10 17:32:07 -0800

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to trainsanywhere
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/trainsanywhere/core_test.clj
@@ -0,0 +1,7 @@
+(ns trainsanywhere.core-test
+  (:require [clojure.test :refer :all]
+            [trainsanywhere.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
