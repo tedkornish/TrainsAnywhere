ae4e7cc582fcfcd6058204f0be3c85b902d70fa1
Ted Kornish <ted.kornish+github@gmail.com>
2016-02-10 18:00:03 -0800

    Add functions for fetching stations from Rail Europe API

--- project.clj
+++ project.clj
@@ -3,7 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.cemerick/url "0.1.1"]
+                 [camel-snake-kebab "0.3.2"]
+                 [org.clojure/data.json "0.2.6"]
+                 [clj-http "2.0.0"]]
   :main ^:skip-aot trainsanywhere.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
---
+++ src/trainsanywhere/rail_europe_api.clj
@@ -0,0 +1,29 @@
+(ns trainsanywhere.rail-europe-api
+  (:require [cemerick.url :refer [url]]
+            [clojure.data.json :as json]
+            [clj-http.client :as client]
+            [camel-snake-kebab.core :refer [->snake_case_keyword]]))
+
+(defn url-for-station-page [n]
+  "Assemble the URL for fetching a page of stations from the Rail Europe API."
+  (-> (url "https://www2.raileurope.com/us/shopping/popular-locations.htm?")
+      (assoc :query {:batchSize 20 :batchNr n})
+      str))
+
+(defn fetch-stations-page [n]
+  "Fetch a single page of stations from the Rail Europe API."
+  (-> n
+      url-for-station-page
+      client/get
+      (get :body)
+      (json/read-str :key-fn ->snake_case_keyword)
+      (get :list)))
+
+(defn fetch-all-stations []
+  "Fetch all stations before returning them in a list. Does not write to DB."
+  (loop [stations []
+         current-page 1]
+    (let [page (fetch-stations-page current-page)]
+      (if (nil? page)
+        stations
+        (recur (concat stations page) (inc current-page))))))
